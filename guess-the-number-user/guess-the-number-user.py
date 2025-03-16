13202d942d90921d467ce469b4fab4b1b22413b7
matheushage1 <matheushagecontato@gmail.com>
2025-03-16 18:33:41 -0300

    adding new exercise

---
+++ guess-the-number-user/guess-the-number-user.py
@@ -0,0 +1,24 @@
+import random
+
+
+def computer_guess(number_to_guess):
+    low = 1
+    high = number_to_guess
+    feedback = ""
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(
+            f"Is {guess} too high (H), too low (L) or correct (C)? ").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(
+        f"Wow, nice one. The computer guessed your number {guess} correctly!")
+
+
+computer_guess(10)
