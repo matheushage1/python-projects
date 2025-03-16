63d7263184ed4ac98f699b3379301132e772a8cf
matheushage1 <matheushagecontato@gmail.com>
2025-03-16 13:26:14 -0300

    adding the guess-number-computer-game

---
+++ guess-the-number-computer/guess-the-number-computer.py
@@ -0,0 +1,19 @@
+import random
+
+
+def guess(number_to_guess):
+    random_number = random.randint(1, number_to_guess)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(
+            f"Try to figure what number is from 1 - {number_to_guess}: "))
+        print(guess)
+        if guess < random_number:
+            print("Sorry, guess again. Too low.")
+        elif guess > random_number:
+            print("Sorry, guess again. Too high")
+
+    print("Yay, congrats. You've guessed the number!")
+
+
+guess(10)
